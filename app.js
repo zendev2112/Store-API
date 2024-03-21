d31ebdbb63235fba02530255e1b786348dc36408
zen <zenon.deviagge@gmail.com>
2024-03-21 18:17:31 -0300

    Populate DB

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
-// async errors
-
+require('express-async-errors');
 const express = require('express');
 
 const app = express();
+
+const connectDB = require('./db/connect');
+
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -17,6 +20,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
+app.use('/api/v1/products', productsRouter);
+
 // products route
 
 app.use(notFoundMiddleware);
@@ -27,6 +32,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
