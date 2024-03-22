53c873126c67779a26e115413e6839045cb8e02f
zen <zenon.deviagge@gmail.com>
2024-03-22 17:44:54 -0300

    Reafactor queryObject in dynamic getAllProducts of controllers.

--- controllers/products.js
+++ controllers/products.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unneeded-ternary */
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
@@ -8,8 +9,15 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
-  res.status(200).json({ products, nbHits: products.length })
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
