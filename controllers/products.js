b375cd37095ea723e41e781c61d277e2d3df3d95
zen <zenon.deviagge@gmail.com>
2024-03-25 15:49:09 -0300

    Add select feature.

--- controllers/products.js
+++ controllers/products.js
@@ -4,12 +4,12 @@ const product = require('../models/product');
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('name price');
+  const products = await Product.find({}).select('name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -23,11 +23,17 @@ const getAllProducts = async (req, res) => {
   }
   // console.log(queryObject);
   let result = Product.find(queryObject);
+  // sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
   }
 
   const products = await result;
