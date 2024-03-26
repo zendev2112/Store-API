b28356d6a60c287e8cd656b7ee0c9ccba64efab2
zen <zenon.deviagge@gmail.com>
2024-03-26 18:48:24 -0300

    Configure pagination with skip and limit.

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,10 @@ const product = require('../models/product');
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('price')
+    .select('name price');
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -35,6 +38,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
